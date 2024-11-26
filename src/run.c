e097ea6aca768485e9d657d9cd50cf2d749325cb
gujoh <gustafjohanson@live.com>
2024-11-26 14:37:22 +0100

    start testing stuff

--- src/run.c
+++ src/run.c
@@ -3,7 +3,9 @@
 #include <stdlib.h>
 #include "lattice.h"
 #include "potential.h"
+#include "tools.h"
 
+void print_positions(double** positions, int n_atoms);
 
 int
 run(
@@ -14,5 +16,27 @@ run(
     // Write your code here
     // This makes it possible to test
     // 100% of you code
+    int n = 4;
+    int n_atoms = 256;
+    double lattice_param = 0.5;
+    double** positions = create_2D_array(n_atoms, 3);
+    init_fcc(positions, n, lattice_param);
+   
+    print_positions(positions, n_atoms);
+
+    double e_pot = get_energy_AL(positions, n, n_atoms);
+    printf("%f", e_pot);
     return 0;
+}
+
+void print_positions(double** positions, int n_atoms)
+{
+ for (int i = 0; i < n_atoms; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            printf("%i: %f\t", j, positions[i][j]);
+        }
+        printf("\n");
+    }
 }
