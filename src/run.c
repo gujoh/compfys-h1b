b00a3569943b5a0b39cebd7ed38294e285473c74
gujoh <gustafjohanson@live.com>
2024-11-26 15:35:45 +0100

    getting somewhere

--- src/run.c
+++ src/run.c
@@ -6,6 +6,7 @@
 #include "tools.h"
 
 void print_positions(double** positions, int n_atoms);
+void get_linspace(double* linspace, double start, double end, int n);
 
 int
 run(
@@ -18,14 +19,21 @@ run(
     // 100% of you code
     int n = 4;
     int n_atoms = 256;
-    double lattice_param = 0.5;
+    double start = 3.9;
+    double end = 4.5;
+    int linspace_len = 1000;
+    double* lattice_params = (double*) malloc(sizeof(double) * linspace_len);
+    get_linspace(lattice_params, start, end, linspace_len);
     double** positions = create_2D_array(n_atoms, 3);
-    init_fcc(positions, n, lattice_param);
    
-    print_positions(positions, n_atoms);
-
-    double e_pot = get_energy_AL(positions, n, n_atoms);
-    printf("%f", e_pot);
+    FILE* file = fopen("data/task1.csv", "w+");
+    for (int i = 0; i < linspace_len; i++)
+    {
+        double lattice_param = lattice_params[i];
+        init_fcc(positions, n, lattice_param);
+        double e_pot = get_energy_AL(positions, n * lattice_param, n_atoms);
+        fprintf(file, "%f,%f\n", lattice_param, e_pot);
+    }
     return 0;
 }
 
@@ -38,5 +46,14 @@ void print_positions(double** positions, int n_atoms)
             printf("%i: %f\t", j, positions[i][j]);
         }
         printf("\n");
+    }
+}
+
+void get_linspace(double* linspace, double start, double end, int n) 
+{
+    double increment = (end - start) / n;
+    for (int i = 0; i < n; i++)
+    {
+        linspace[i] = start + i * increment;
     }
 }
